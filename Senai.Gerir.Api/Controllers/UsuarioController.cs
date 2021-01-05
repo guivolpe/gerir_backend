70e4e9adb8eb1358e1515895bc83c162a90a9c9a
guivolpe <54940395+guivolpe@users.noreply.github.com>
2021-01-05 09:41:46 -0300

    controler&repositorio

---
+++ Senai.Gerir.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Senai.Gerir.Api.Dominios;
+using Senai.Gerir.Api.Interfaces;
+using Senai.Gerir.Api.Repositorios;
+
+namespace Senai.Gerir.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController()
+        {
+            _usuarioRepositorio = new UsuarioRepositorio();
+        }
+
+        [HttpPost]
+        //retorno     nome do endpoint    (parâmetro)
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            try
+            {
+                _usuarioRepositorio.Cadastrar(usuario);
+
+
+                return Ok(usuario);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
+        }
+        [HttpPost ("login")]
+        public IActionResult Logar(Usuario usuario)
+        {
+            try
+            {
+                var usuarioexiste = _usuarioRepositorio.Logar(usuario.Email, usuario.Senha);
+
+                //caso o usuario não exista
+                if (usuarioexiste == null)
+                    return NotFound();
+                //caso exista
+                return Ok(usuarioexiste);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
